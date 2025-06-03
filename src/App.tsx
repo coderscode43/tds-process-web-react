47d576a75ef77a46eb3ab9b500c3e897a8a48250
Abhishek D Jaiwar <abhishekjaiswar@gmail.com>
2025-06-03 11:23:45 +0530

    Initailize shadcn/ui component library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <>
       <div className="h-100vh w-100vw bg-amber-600 text-center text-2xl">
         <h1 className="text-white">Hello World!</h1>
+      </div>
+      <div className="flex justify-center align-middle">
+        <Button className="bg-violet-600 font-semibold text-white">
+          Submit
+        </Button>
       </div>
     </>
   );
